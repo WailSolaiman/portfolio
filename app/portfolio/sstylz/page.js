ab98263adfc2c20255b385a3c9a45b1571d1d295
Wail Solaiman <wailsolaiman@hotmail.com>
2024-09-30 17:33:53 +0200

    add animation to portfolio items, edit hero component main container width

--- app/portfolio/sstylz/page.js
+++ app/portfolio/sstylz/page.js
@@ -1,9 +1,10 @@
 'use client'
 
+import { motion } from 'framer-motion'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 
-import { FaBackward } from 'react-icons/fa6'
+import { IoArrowBackCircleOutline } from 'react-icons/io5'
 import { FiExternalLink } from 'react-icons/fi'
 import { FaWordpress } from 'react-icons/fa6'
 import { SiWoocommerce } from 'react-icons/si'
@@ -12,71 +13,150 @@ import { FaCss3 } from 'react-icons/fa6'
 import { SiJavascript } from 'react-icons/si'
 
 import sstylzImg from '@/public/sstylz.jpg'
+import sstylzLogoImg from '@/public/sstylz-logo.png'
 
 const SStylz = () => {
 	const router = useRouter()
-	return (
-		<div className='min-h-[100vh] bg-gradient-to-b from-stone-900 to-red-900'>
-			<div className='container w-3/4 xl:w-2/4 mx-auto pt-10'>
-				<button
-					type='button'
-					className='mb-8'
-					onClick={() => router.back()}>
-					<FaBackward />
-				</button>
-				<p>Logo</p>
-				<p className='rammetto text-3xl mb-4'>
-					<a href='#' target='_blank' className='underline flex'>
-						SStylz <FiExternalLink size={20} />
-					</a>
-				</p>
-				<div className='grid grid-cols-1 md:grid-cols-2'>
-					<div>
-						<p className='roboto-light text-sm mb-4 w-full xl:w-1/2'>
-							Developed a modern e-commerce website for SStylz.
-						</p>
-						<p className='rammetto text-sm'>Technologies used:</p>
 
-						<div className='flex flex-row justify-start items-center mr-6'>
-							<span className='mr-4'>
-								<FaWordpress size={40} />
-							</span>
-							<span className='mr-4'>
-								<SiWoocommerce size={70} />
-							</span>
-							<span className='mr-4'>
-								<FaHtml5 size={40} />
-							</span>
-							<span className='mr-4'>
-								<FaCss3 size={40} />
-							</span>
-							<span className='mr-4'>
-								<SiJavascript size={40} />
-							</span>
-						</div>
-					</div>
-					<div className='roboto-light text-sm w-full'>
-						<p className='mb-4'>
-							Developed a modern e-commerce website for SStylz, a
-							women's clothing store. The site features an
-							intuitive design, seamless shopping experience, and
-							responsive layout tailored to showcase fashion
-							collections effectively.
+	const tech = [
+		<FaWordpress size={40} />,
+		<SiWoocommerce size={60} />,
+		<FaHtml5 size={40} />,
+		<FaCss3 size={40} />,
+		<SiJavascript size={40} />,
+	]
+
+	return (
+		<div className='min-h-[100vh] bg-gradient-to-b from-stone-900 to-red-900 flex'>
+			<div className='container w-11/12 lg:w-10/12 xl:w-8/12 m-auto'>
+				<div className='grid grid-cols-1 lg:grid-cols-2 gap-6 py-4'>
+					<div>
+						<button
+							type='button'
+							className='mb-8'
+							onClick={() => router.back()}>
+							<IoArrowBackCircleOutline size={50} />
+						</button>
+
+						<motion.div
+							initial={{ opacity: 0, y: -5 }}
+							whileInView={{ opacity: 1, y: 0 }}
+							viewport={{ once: true }}
+							transition={{
+								duration: 0.5,
+								ease: 'easeInOut',
+							}}>
+							<Image
+								src={sstylzLogoImg}
+								alt='sstylz'
+								width={100}
+								className='h-auto'
+								priority
+							/>
+						</motion.div>
+						<p className='rammetto my-6'>
+							<a
+								href='https://sstylz.store/'
+								target='_blank'
+								className='underline flex'>
+								<span className='text-5xl'>SStylz</span>{' '}
+								<FiExternalLink size={20} />
+							</a>
 						</p>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing
-							elit.
-						</p>
+						<motion.div
+							initial={{ opacity: 0, y: -5 }}
+							whileInView={{ opacity: 1, y: 0 }}
+							viewport={{ once: true }}
+							transition={{
+								duration: 0.5,
+								delay: 0.2,
+								ease: 'easeInOut',
+							}}>
+							<p className='roboto-bold text-sm mb-4 w-full'>
+								SStylz®: Where Fashion Flourishes.
+							</p>
+							<p className='roboto-light text-sm my-4 w-full'>
+								Developed a modern e-commerce website for
+								SStylz, a women's clothing store. The site
+								features an intuitive design, seamless shopping
+								experience, and responsive layout tailored to
+								showcase fashion collections effectively.
+							</p>
+						</motion.div>
+						<p className='rammetto text-sm'>Technologies used:</p>
+						<motion.div className='flex flex-row justify-start items-center'>
+							{tech.map((item, index) => (
+								<motion.span
+									key={index}
+									className='mr-4'
+									initial={{ opacity: 0, x: -5 }}
+									whileInView={{ opacity: 1, x: 0 }}
+									viewport={{ once: true }}
+									transition={{
+										duration: 0.5,
+										delay: 0.2 * index,
+										ease: 'easeInOut',
+									}}>
+									{item}
+								</motion.span>
+							))}
+						</motion.div>
+
+						<motion.div
+							className='w-full'
+							initial={{ opacity: 0, y: -5 }}
+							whileInView={{ opacity: 1, y: 0 }}
+							viewport={{ once: true }}
+							transition={{
+								duration: 0.5,
+								delay: 0.5,
+								ease: 'easeInOut',
+							}}>
+							<Image
+								src={sstylzLogoImg}
+								alt='sstylz'
+								width={300}
+								className='h-auto mb-4'
+								priority
+							/>
+						</motion.div>
+
+						<motion.div
+							className='w-full'
+							initial={{ opacity: 0, y: -5 }}
+							whileInView={{ opacity: 1, y: 0 }}
+							viewport={{ once: true }}
+							transition={{
+								duration: 0.5,
+								delay: 0.6,
+								ease: 'easeInOut',
+							}}>
+							<p className='roboto-light text-sm'>
+								Unveil your unique style with our curated
+								collection of unisex fashion essentials. From
+								trendy accessories to versatile clothing and
+								shoes, we redefine fashion, empowering you to
+								express your individuality with confidence.
+							</p>
+						</motion.div>
 					</div>
-				</div>
-				<div className='flex justify-center pt-6'>
-					<Image
-						src={sstylzImg}
-						alt='sstylz'
-						width={600}
-						height='auto'
-						priority
-					/>
+					<motion.div
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						transition={{
+							duration: 0.5,
+							delay: 0.5,
+							ease: 'easeInOut',
+						}}
+						viewport={{ once: true }}
+						className='w-full min-h-[350px] sm:min-h-[500px] md:min-h-[600px] 
+						rounded-2xl shadow-sm'
+						style={{
+							backgroundImage: `url(${sstylzImg.src})`,
+							backgroundRepeat: 'no-repeat',
+							backgroundPosition: 'center',
+							backgroundSize: 'cover',
+						}}></motion.div>
 				</div>
 			</div>
 		</div>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,16 +4,16 @@ const Hero = () => {
 	const { hero } = data
 
 	return (
-		<div className='container w-3/4 mx-auto mt-10'>
+		<div className='container w-10/12 xl:w-8/12 m-auto mt-20'>
 			<div className='grid grid-cols-1 lg:grid-cols-[auto_1fr] gap-2 lg:gap-16'>
 				<div>
-					<p className='rammetto text-3xl'>
+					<p className='rammetto text-3xl lg:text-5xl'>
 						{hero.firstname} <br />
 						{hero.lastname}{' '}
 					</p>
 				</div>
 				<div>
-					<p className='rammetto w-full xl:w-1/2 text-md mt-3 lg:mt-0'>
+					<p className='rammetto w-full xl:w-1/2 text-xl mt-3 lg:mt-0'>
 						{hero.about}
 					</p>
 				</div>
